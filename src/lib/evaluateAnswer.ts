4b630c432c1daf47444b20d8924ef17436d49055
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-13 20:27:03 +0300

    Create `evaluateAnswer` function
    This function takes user's answer to a question and
    makes the LLM evaluates the answer and gives feedbacks
    to the user if his answer is wrong or incomplete.

---
+++ src/lib/evaluateAnswer.ts
@@ -0,0 +1,41 @@
+import { GenerateQuestionsParams } from './generateQuestions';
+import { modelName } from '@/constants/model';
+import ollamaClient from './ollamaClient';
+
+type EvaluateAnswerParams = {
+    answer: string;
+    question: string;
+} & Pick<GenerateQuestionsParams, "field" | "topic">
+
+export default async function evaluateAnswer({ field, topic, answer, question }: EvaluateAnswerParams) {
+    const response = await ollamaClient.chat({
+        model: modelName,
+        messages: [
+            {
+                role: "system",
+                content: `
+                    You are an expert in ${field},
+                    Your task is to assess user's knowledge in ${field}, specifically in ${topic} topic.
+                    evaluate user's answer to "${question}" question in ${topic} topic whether it is right or wrong,
+                    correct the user if his answer is wrong, incomplete or lacks something.
+                    Make your correction to the point and directed to the user as you talk to him.
+                `,
+            },
+            {
+                role: "user",
+                content: answer,
+            }
+        ],
+        stream: true,
+    })
+
+    const stream = new ReadableStream({
+        async start(controller) {
+            for await (const llmResponsePart of response) {
+                controller.enqueue(llmResponsePart.message.content)
+            }
+        },
+    })
+
+    return stream
+};
