af440ec11738ab883a0c9839065ec5c4688fbe07
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-18 21:22:35 +0300

    Build tests page

--- src/app/(pages)/specializations/[id]/[item]/tests-page/page.tsx
+++ src/app/(pages)/specializations/[id]/[item]/tests-page/page.tsx
@@ -4,6 +4,11 @@ import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import PageLayout from '@/components/PageLayout';
 import Back from '@/components/Back';
 import useQuestions from '@/hooks/useQuestions';
+import TrueOrFalseTest from '@/components/TrueOrFalseTest';
+import EssayTest from '@/components/EssayTest';
+import DisscusionArea from '@/components/DisscusionArea';
+import MultiOptionsTest from '@/components/MultiOptionsTest';
+import useAnswer from '@/hooks/useAnswer';
 
 const TestingPage = () => {
     const router = useRouter();
@@ -14,19 +19,97 @@ const TestingPage = () => {
     const level = decodeURIComponent(searchParams.get("level") || "medium") as QuestionLevel
     const questionType = decodeURIComponent(searchParams.get("question-type") || "multi options") as QuestionType
 
-    const { loading, error, questions } = useQuestions({
+    const {
+        loading: questionsLoading,
+        error: questionsError,
+        questions,
+        currentQuestionIndex,
+        goToNextQuestion,
+    } = useQuestions({
         field,
         topic,
         level,
         questionType,
     })
 
-    console.log(questions)
+    const currentQuestion =
+        typeof questions[currentQuestionIndex] === "string" ? questions[currentQuestionIndex]
+            : questions[currentQuestionIndex]?.question
+
+    const {
+        submitAnswer,
+        loading: answerLoading,
+        error: answerError,
+        response,
+        done: responseDone,
+        discussionId,
+        chosenAnswer,
+    } = useAnswer({
+        field,
+        topic,
+        question: currentQuestion,
+        questionType,
+    })
 
     return (
         <PageLayout>
-            <Back onClick={() => router.replace(`/specializations/${field}/${topic}`)} />
-            {/* Here we render the components of tests based on te type of the test */}
+            <div className='flex flex-col space-y-10'>
+                {
+                    questionsLoading ? "Loading..." :
+                        questionsError ? "Error !!!" :
+                            questionType === "multi options" ? (
+                                <MultiOptionsTest
+                                    multiOptionsQuestion={questions[currentQuestionIndex] as MultiOptionsQuestion}
+                                    field={field}
+                                    topic={topic}
+                                    submitAnswer={submitAnswer}
+                                    questionNumber={currentQuestionIndex + 1}
+                                />
+                            ) :
+                                questionType === "true or false" ? (
+                                    <TrueOrFalseTest
+                                        field={field}
+                                        topic={topic}
+                                        questionNumber={currentQuestionIndex + 1}
+                                        question={questions[currentQuestionIndex] as string}
+                                        submitAnswer={submitAnswer}
+                                    />
+                                ) : questionType === "essay" ? (
+                                    <EssayTest
+                                        field={field}
+                                        topic={topic}
+                                        questionNumber={currentQuestionIndex + 1}
+                                        question={questions[currentQuestionIndex] as string}
+                                        submitAnswer={submitAnswer}
+                                    />
+                                ) : null
+                }
+                {
+                    answerLoading || response &&
+                    <DisscusionArea
+                        question={currentQuestion}
+                        field={field}
+                        topic={topic}
+                        questionType={questionType}
+                        discussionId={discussionId}
+                        firstResponse={{
+                            answer: response,
+                            isGenerating: answerLoading,
+                            error: answerError,
+                            generatingDone: responseDone,
+                        }}
+                    />
+                }
+                {
+                    responseDone &&
+                    <button
+                        className="bg-crystalTeal text-white px-8 py-3 w-fit rounded-lg mb-4 mx-auto cursor-pointer"
+                        onClick={goToNextQuestion}
+                    >
+                        Next Question
+                    </button>
+                }
+            </div>
         </PageLayout>
     )
 }
