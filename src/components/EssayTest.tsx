4be3b287f4c2a394986c19bdde7701bce4052d4f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-18 05:00:06 +0300

    Create `EssayTest` component

---
+++ src/components/EssayTest.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { FormEvent } from "react";
+
+type EssayTestProps = {
+    question: string;
+    questionNumber: number;
+    field: string;
+    topic: string;
+}
+
+export default function EssayTest({ question, questionNumber, field, topic }: EssayTestProps) {
+
+    function onSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const answer = form.get("answer")
+        if (answer) {
+            console.log(answer)
+        }
+    }
+
+    return (
+        <div className="space-y-8">
+            <h1 className="text-white flex text-2xl gap-2">
+                <span>{questionNumber}.</span> {question}
+            </h1>
+            <form onSubmit={onSubmit}>
+                <textarea
+                    className="w-full px-2 py-1.5 rounded-lg focus:outline-none transition max-h-40"
+                    name="answer"
+                    placeholder="Type your answer here..."
+                    rows={3}
+                ></textarea>
+                <button className="bg-crystalTeal float-right text-white px-6 py-1 rounded-lg cursor-pointer" type="submit">
+                    Send
+                </button>
+            </form>
+        </div>
+    );
+}
