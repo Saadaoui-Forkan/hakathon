cba29232d2364928cfe927fb31b3357e3f1f1bcb
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-18 05:19:57 +0300

    Init `MultiOptionsTest` component

---
+++ src/components/MultiOptionsTest.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+type MultiOptionsTestProps = {
+    field: string;
+    topic: string;
+    multiOptionsQuestion: MultiOptionsQuestion
+}
+
+export default function MultiOptionsTest({ multiOptionsQuestion, field, topic }: MultiOptionsTestProps) {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
