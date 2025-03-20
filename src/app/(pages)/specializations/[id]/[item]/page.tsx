925da664588441b2b15af20e80f71b8f4ade0f9a
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-20 23:57:27 +0300

    Refactor test properties selection page

--- src/app/(pages)/specializations/[id]/[item]/page.tsx
+++ src/app/(pages)/specializations/[id]/[item]/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 const QuizSelection = () => {
     const params = useParams<{ item: string, id: string }>();
     const topic = decodeURIComponent(params.item)
+    const field = decodeURIComponent(params.id)
     const router = useRouter();
 
     const [selectionState, setSelectionState] = useState<{ level: QuestionLevel | "", questionType: QuestionType | "" }>({
@@ -19,8 +20,12 @@ const QuizSelection = () => {
 
     return (
         <PageLayout>
-            <Back onClick={() => router.replace(`/specializations/${params.id}`)} />
-            <h2 className="text-white text-2xl font-bold mb-2">Select the level of the test</h2>
+            <Back onClick={() => router.replace(`/specializations/${field}`)} />
+            <div className='mb-5'>
+                <h2 className="text-white text-2xl font-bold mb-1">{field}</h2>
+                <p className='text-white'>{topic}</p>
+            </div>
+            <h2 className="text-white text-xl mb-1">Select the level of the test</h2>
             <GridContainer>
                 {levels.map(level => (
                     <button
@@ -32,7 +37,7 @@ const QuizSelection = () => {
                     </button>
                 ))}
             </GridContainer>
-            <h2 className="text-white text-2xl font-bold mb-2 mt-4">Select the type of the test</h2>
+            <h2 className="text-white text-xl mb-1 mt-4">Select the type of the test</h2>
             <GridContainer>
                 {questionTypes.map(type => (
                     <button
@@ -52,7 +57,7 @@ const QuizSelection = () => {
                         if (selectionState.level && selectionState.questionType) {
                             query.set("level", selectionState.level)
                             query.set("question-type", selectionState.questionType)
-                            router.push(`/specializations/${params.id}/${topic}/tests-page?${query.toString()}`)
+                            router.push(`/specializations/${field}/${topic}/tests-page?${query.toString()}`)
                         }
                     }}
                 >
