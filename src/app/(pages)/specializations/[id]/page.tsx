571810e4daa70a8c367b47eec3f6dd28e93be186
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-03-20 21:37:14 +0300

    Refactor `src/app/(pages)/specializations/[id]` page

--- src/app/(pages)/specializations/[id]/page.tsx
+++ src/app/(pages)/specializations/[id]/page.tsx
@@ -12,18 +12,18 @@ import React from 'react'
 const Topic = () => {
   const params = useParams<{ id: string }>();
   const router = useRouter();
-  const topic = decodeURIComponent(params.id)
-  const currentSpecialization = specializationFields.find(el => el.fieldName === topic)
+  const field = decodeURIComponent(params.id)
+  const currentSpecialization = specializationFields.find(el => el.fieldName === field)
 
   return (
     <PageLayout>
       <Back onClick={() => router.replace("/specializations")} />
       <h1 className="text-white text-2xl font-bold">Choose the topic of the test</h1>
-      <p className="text-gray-200 mb-4">{topic}</p>
+      <p className="text-gray-200 mb-4">{field}</p>
       {currentSpecialization && (
         <GridContainer>
           {currentSpecialization.topics.map((el, index) => (
-            <Link key={index} href={`/specializations/${topic}/${el}`} className="bg-electricMagenta text-pureWhite px-4 py-2 rounded-lg">
+            <Link key={index} href={`/specializations/${field}/${el}`} className="bg-electricMagenta text-pureWhite px-4 py-2 rounded-lg">
               {el}
             </Link>
           ))}
@@ -32,7 +32,7 @@ const Topic = () => {
       <ChoiceInput
         title={`${currentSpecialization ? "Or " : ""}type a topic inside your field of specialization`}
         inputPlaceHolder='Type the topic here...'
-        onChoice={(choice) => router.replace(`/specializations/${topic}/${choice}`)}
+        onChoice={(choice) => router.replace(`/specializations/${field}/${choice}`)}
       />
     </PageLayout>
   )
